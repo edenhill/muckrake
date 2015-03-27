8d65a231f43e19c980cd66d92b51973c51a08925
Geoff Anderson <granders@gmail.com>
2015-03-27 20:18:22 +0000

    Initial commit.

---
+++ aws/aws-init.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# This script should be run once on your aws test driver machine before
+# attempting to run any ducttape tests
+
+# Install dependencies
+sudo apt-get install -y maven openjdk-6-jdk build-essential \
+            ruby-dev zlib1g-dev realpath
+
+base_dir=`dirname $0`/..
+
+if [ -z `which vagrant` ]; then
+    echo "Installing vagrant..."
+    wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+    sudo dpkg -i vagrant_1.7.2_x86_64.deb
+    rm -f vagrant_1.7.2_x86_64.deb
+fi
+
+# Install necessary vagrant plugins
+# Note: Do NOT install vagrant-cachier since it doesn't work on AWS and only
+# adds log noise
+vagrant_plugins="vagrant-aws vagrant-hostmanager"
+existing=`vagrant plugin list`
+for plugin in $vagrant_plugins; do
+    echo $existing | grep $plugin > /dev/null
+    if [ $? != 0 ]; then
+        vagrant plugin install $plugin
+    fi
+done
+
+# Create Vagrantfile.local as a convenience
+if [ ! -e "$base_dir/Vagrantfile.local" ]; then
+    cp aws-example-Vagrantfile.local $base_dir/Vagrantfile.local
+fi
+
+gradle="gradle-2.2.1"
+if [ -z `which gradle` ] && [ ! -d $base_dir/$gradle ]; then
+    if [ ! -e $gradle-bin.zip ]; then
+        wget https://services.gradle.org/distributions/$gradle-bin.zip
+    fi
+    unzip $gradle-bin.zip
+    rm -rf $gradle-bin.zip
+    mv $gradle $base_dir/$gradle
+fi
+
+# Ensure aws access keys are in the environment
+grep "AWS ACCESS KEYS" ~/.bashrc > /dev/null
+if [ $? != 0 ]; then
+  echo "# --- AWS ACCESS KEYS ---" >> ~/.bashrc
+  echo ". `realpath aws-access-keys-commands`" >> ~/.bashrc
+  echo "# -----------------------" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+$base_dir/build.sh --aws
